cbacf5efcc3db514528ef116c9a9a3f1e8769f2e
Rogerz Zhang <rogerz.zhang@gmail.com>
2013-10-03 11:39:50 +0800

    Update css setting

--- custom.el
+++ custom.el
@@ -3,6 +3,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(css-indent-offset 2)
  '(ido-enable-flex-matching t)
  '(package-archives (quote (("gnu" . "http://elpa.gnu.org/packages/") ("marmalade" . "http://marmalade-repo.org/packages/") ("melpa" . "http://melpa.milkbox.net/packages/")))))
 (custom-set-faces
